cc02e3e69d08d70d60491e5b3bf0601cf855258e
Fabian Schneider <fabi@celine.local>
2018-04-30 13:40:33 +0200

    Initial commit...

---
+++ DataAccess/Fabi.Rest.Api.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Fabi.Rest.Api.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Domain/Fabi.Rest.Api.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Fabi.Rest.Api.Domain
+{
+    public class Class1
+    {
+    }
+}
